807fb7e898131ac8c33be84beff8b31029adc20b
aquoss <amberquoss@gmail.com>
2017-01-25 17:03:41 -0800

    finishes pet show and add

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,6 +8,18 @@ class PetsController < ApplicationController
     @pet = Pet.find_by(id: params[:id])
   end
 
+  def new
+    @pet = Pet.new
+    # @pet.owner_id = @owner.id
+  end
+
+  def create
+    pet = Pet.create(pet_params)
+    @owner = Owner.find_by(id: params[:owner_id])
+    @owner.pets.push(pet)
+    redirect_to pet_path(pet)
+  end
+
   # TODO: set up *new* method with data for new view
 
   # TODO: set up *create* method with database interactions for create
@@ -15,7 +27,7 @@ class PetsController < ApplicationController
 
   private
   def pet_params
-    params.require(:pet).permit(:name, :breed, :date_of_birth)
+    params.require(:pet).permit(:name, :breed)
   end
 
 end
